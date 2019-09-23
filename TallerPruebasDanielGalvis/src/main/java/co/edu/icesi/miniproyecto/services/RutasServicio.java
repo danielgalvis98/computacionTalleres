c4a5cf508b54e8a55f0501bbfc2b99ac6f5519a6
danielgalvis98 <dangaltor@gmail.com>
2019-09-23 17:25:37 -0500

    Unit Tests add Rutas

--- TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/RutasServicio.java
+++ TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/RutasServicio.java
@@ -7,7 +7,7 @@ import co.edu.icesi.miniproyecto.model.Tmio1Ruta;
 
 public interface RutasServicio {
 
-	public void addRuta (Tmio1Ruta ruta);
+	public void addRuta (Tmio1Ruta ruta) throws Exception;
 	
 	public Tmio1Ruta deleteRuta (Tmio1Ruta ruta);
 	
--- TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/RutasServicioImpl.java
+++ TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/RutasServicioImpl.java
@@ -3,17 +3,34 @@ package co.edu.icesi.miniproyecto.services;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.edu.icesi.miniproyecto.model.Tmio1Ruta;
+import co.edu.icesi.miniproyecto.repository.RutasRepository;
 
 @Service
 public class RutasServicioImpl implements RutasServicio{
 
+	private RutasRepository repository;
+	
+	@Autowired
+	public RutasServicioImpl(RutasRepository repo) {
+		repository = repo;
+	}
+	
 	@Override
-	public void addRuta(Tmio1Ruta ruta) {
-		// TODO Auto-generated method stub
+	public void addRuta(Tmio1Ruta ruta) throws Exception {
+		if (ruta == null) {
+			throw new NullPointerException("La ruta no puede ser nula");
+		}
+		if (ruta.getDiaFin().compareTo(ruta.getDiaInicio()) < 0)
+			throw new Exception("El día de inicio debe ser menor al día de fin");
 		
+		if (ruta.getHoraFin().compareTo(ruta.getHoraInicio()) < 0) {
+			throw new Exception("La hora de inicio ser menor la hora de fin");
+		}
+		repository.addRuta(ruta);
 	}
 
 	@Override
