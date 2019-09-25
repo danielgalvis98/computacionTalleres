762f0669a325e46152c94416e7029d5f64cced9e
danielgalvis98 <dangaltor@gmail.com>
2019-09-24 19:30:03 -0500

    First service of services corrected

--- TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/ServiciosServicioImpl.java
+++ TallerPruebasDanielGalvis/src/main/java/co/edu/icesi/miniproyecto/services/ServiciosServicioImpl.java
@@ -5,7 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.icesi.miniproyecto.model.Tmio1Bus;
+import co.edu.icesi.miniproyecto.model.Tmio1Conductore;
+import co.edu.icesi.miniproyecto.model.Tmio1Ruta;
 import co.edu.icesi.miniproyecto.model.Tmio1Servicio;
+import co.edu.icesi.miniproyecto.repository.BusesRepository;
+import co.edu.icesi.miniproyecto.repository.ConductoresRepository;
+import co.edu.icesi.miniproyecto.repository.RutasRepository;
 import co.edu.icesi.miniproyecto.repository.ServiciosRepository;
 
 @Service
@@ -13,9 +19,19 @@ public class ServiciosServicioImpl implements ServiciosServicio{
 
 	private ServiciosRepository repository;
 	
+	private BusesRepository busesRepository;
+	
+	private ConductoresRepository conductoresRepository;
+	
+	private RutasRepository rutasRepository;
+	
 	@Autowired
-	public ServiciosServicioImpl(ServiciosRepository repo) {
+	public ServiciosServicioImpl(ServiciosRepository repo, BusesRepository busRepo,
+			ConductoresRepository conductoresRepo, RutasRepository rutasRepo) {
 		repository = repo;
+		busesRepository = busRepo;
+		conductoresRepository = conductoresRepo;
+		rutasRepository = rutasRepo;
 	}
 	@Override
 	public void addServicio(Tmio1Servicio servicio) throws Exception {
@@ -27,11 +43,26 @@ public class ServiciosServicioImpl implements ServiciosServicio{
 			throw new Exception("El bus debe de estar registrado");
 		}
 		
+		Tmio1Bus bus = busesRepository.getBus(servicio.getTmio1Bus().getId()) ;
+		if (bus == null) {
+			throw new Exception("El bus debe de estar registrado");
+		}
+		
 		if (servicio.getTmio1Conductore() == null) {
+			throw new Exception("El conductor debe de estar registrado");
+		}
+		Tmio1Conductore conductor = conductoresRepository.getConductor(servicio.getTmio1Conductore().getCedula());
+		
+		if (conductor == null) {
 			throw new Exception("El conductor debe de estar registrado");
 		}
 		
 		if (servicio.getTmio1Ruta() == null) {
+			throw new Exception("La ruta debe de estar registrada");
+		}
+		
+		Tmio1Ruta ruta = rutasRepository.getRutas(servicio.getTmio1Ruta().getId());
+		if (ruta == null) {
 			throw new Exception("La ruta debe de estar registrada");
 		}
 		
--- TallerPruebasDanielGalvis/src/test/java/com/taller3/sistemamio/unit_tests/ServiciosServicioTest.java
+++ TallerPruebasDanielGalvis/src/test/java/com/taller3/sistemamio/unit_tests/ServiciosServicioTest.java
@@ -2,6 +2,13 @@ package com.taller3.sistemamio.unit_tests;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,6 +21,9 @@ import co.edu.icesi.miniproyecto.model.Tmio1Bus;
 import co.edu.icesi.miniproyecto.model.Tmio1Conductore;
 import co.edu.icesi.miniproyecto.model.Tmio1Ruta;
 import co.edu.icesi.miniproyecto.model.Tmio1Servicio;
+import co.edu.icesi.miniproyecto.repository.BusesRepository;
+import co.edu.icesi.miniproyecto.repository.ConductoresRepository;
+import co.edu.icesi.miniproyecto.repository.RutasRepository;
 import co.edu.icesi.miniproyecto.repository.ServiciosRepository;
 import co.edu.icesi.miniproyecto.services.ServiciosServicioImpl;
 
@@ -22,18 +32,76 @@ public class ServiciosServicioTest {
 	@Mock
 	private ServiciosRepository repository;
 	
+	@Mock
+	private RutasRepository rutasRepository;
+	
+	@Mock
+	private ConductoresRepository conductoresRepo;
+	
+	@Mock
+	private BusesRepository busesRepo;
+	
 	@InjectMocks
 	private ServiciosServicioImpl service;
 	
 	private Tmio1Bus bus1;
+	private Tmio1Bus bus2;
+	
 	private Tmio1Conductore conductore1;
+	private Tmio1Conductore conductore2;
+	
 	private Tmio1Ruta ruta1;
+	private Tmio1Ruta ruta2;
 	
 	@BeforeClass
-	public void instantiateRelationships() {
+	public void instantiateRelationships() throws ParseException {
 		bus1 = new Tmio1Bus();
+		bus1.setId(0);
+		bus1.setMarca("Volkswagen");
+		bus1.setModelo(new BigDecimal(10));
+		bus1.setPlaca("ABC");
+		bus1.setTipo("T");
+		
+		bus2 = new Tmio1Bus();
+		bus2.setId(1);
+		bus2.setMarca("Volkswagen");
+		bus2.setModelo(new BigDecimal(10));
+		bus2.setPlaca("DEF");
+		bus2.setTipo("P");
+		
 		conductore1 = new Tmio1Conductore();
+		conductore1.setCedula("1");
+		conductore1.setApellidos("a");
+		conductore1.setFechaNacimiento((new SimpleDateFormat("dd/MM/yyyy")).parse("09/22/1980"));
+		conductore1.setFechaContratacion((new SimpleDateFormat("dd/MM/yyyy")).parse("09/22/2019"));
+		conductore1.setNombre("Arroyo");
+		
+		conductore2 = new Tmio1Conductore();
+		conductore2.setCedula("2");
+		conductore2.setApellidos("a");
+		conductore2.setFechaNacimiento((new SimpleDateFormat("dd/MM/yyyy")).parse("09/22/1980"));
+		conductore2.setFechaContratacion((new SimpleDateFormat("dd/MM/yyyy")).parse("09/22/2019"));
+		conductore2.setNombre("Arroyo");
+		
 		ruta1 = new Tmio1Ruta();
+		ruta1.setId(0);
+		ruta1.setActiva("Y");
+		ruta1.setDescripcion("test");
+		ruta1.setDiaFin(new BigDecimal(5));
+		ruta1.setDiaInicio(new BigDecimal(1));
+		ruta1.setHoraFin(new BigDecimal(82800));
+		ruta1.setHoraInicio(new BigDecimal(18000));
+		ruta1.setNumero("T31");
+
+		ruta2 = new Tmio1Ruta();
+		ruta2.setId(1);
+		ruta2.setActiva("Y");
+		ruta2.setDescripcion("test");
+		ruta2.setDiaFin(new BigDecimal(5));
+		ruta2.setDiaInicio(new BigDecimal(1));
+		ruta2.setHoraFin(new BigDecimal(82800));
+		ruta2.setHoraInicio(new BigDecimal(18000));
+		ruta2.setNumero("T31");
 	}
 	
 	@BeforeMethod(alwaysRun = true)
@@ -43,6 +111,10 @@ public class ServiciosServicioTest {
 	
 	@Test
 	public void testAddServiceCorect() throws Exception {
+		when(rutasRepository.getRutas(ruta1.getId())).thenReturn(ruta1);
+		when(busesRepo.getBus(bus1.getId())).thenReturn(bus1);
+		when(conductoresRepo.getConductor(conductore1.getCedula())).thenReturn(conductore1);
+		
 		Tmio1Servicio serv = new Tmio1Servicio();
 		serv.setNewId("0");
 		serv.setTmio1Bus(bus1);
@@ -50,40 +122,92 @@ public class ServiciosServicioTest {
 		serv.setTmio1Ruta(ruta1);
 		service.addServicio(serv);
 		verify(repository).addServicio(serv);
+		verify(rutasRepository).getRutas(ruta1.getId());
+		verify(busesRepo).getBus(bus1.getId());
+		verify(conductoresRepo).getConductor(conductore1.getCedula());
+		
 		verifyNoMoreInteractions(repository);
+		verifyNoMoreInteractions(rutasRepository);
+		verifyNoMoreInteractions(busesRepo);
+		verifyNoMoreInteractions(conductoresRepo);
 	}
 	
-	@Test(expectedExceptions = Exception.class,
-			expectedExceptionsMessageRegExp = "El bus debe de estar registrado")
-	public void testAddServiceNullBus() throws Exception {
+	@Test
+	public void testAddServiceNullBus(){
+		when(rutasRepository.getRutas(ruta1.getId())).thenReturn(ruta1);
+		when(busesRepo.getBus(bus2.getId())).thenReturn(null);
+		when(conductoresRepo.getConductor(conductore1.getCedula())).thenReturn(conductore1);
+		
 		Tmio1Servicio serv = new Tmio1Servicio();
 		serv.setNewId("0");
-		serv.setTmio1Bus(null);
+		serv.setTmio1Bus(bus2);
 		serv.setTmio1Conductore(conductore1);
 		serv.setTmio1Ruta(ruta1);
-		service.addServicio(serv);
+		try {
+			service.addServicio(serv);
+			fail();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			assertEquals(e.getMessage(), "El bus debe de estar registrado");
+			verify(busesRepo).getBus(bus2.getId());
+			verifyNoMoreInteractions(repository);
+			verifyNoMoreInteractions(rutasRepository);
+			verifyNoMoreInteractions(busesRepo);
+			verifyNoMoreInteractions(conductoresRepo);
+		}
 	}
 	
-	@Test(expectedExceptions = Exception.class,
-			expectedExceptionsMessageRegExp = "El conductor debe de estar registrado")
-	public void testAddServiceNullConductor() throws Exception {
+	@Test
+	public void testAddServiceNullConductor(){
+		when(rutasRepository.getRutas(ruta1.getId())).thenReturn(ruta1);
+		when(busesRepo.getBus(bus1.getId())).thenReturn(bus1);
+		when(conductoresRepo.getConductor(conductore2.getCedula())).thenReturn(null);
+		
 		Tmio1Servicio serv = new Tmio1Servicio();
 		serv.setNewId("0");
 		serv.setTmio1Bus(bus1);
-		serv.setTmio1Conductore(null);
+		serv.setTmio1Conductore(conductore2);
 		serv.setTmio1Ruta(ruta1);
-		service.addServicio(serv);
+		try {
+			service.addServicio(serv);
+			fail();
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "El conductor debe de estar registrado");
+			verify(conductoresRepo).getConductor(conductore2.getCedula());
+			verify(busesRepo).getBus(bus1.getId());
+			
+			verifyNoMoreInteractions(repository);
+			verifyNoMoreInteractions(rutasRepository);
+			verifyNoMoreInteractions(busesRepo);
+			verifyNoMoreInteractions(conductoresRepo);
+		}
 	}
 	
-	@Test(expectedExceptions = Exception.class,
-			expectedExceptionsMessageRegExp = "La ruta debe de estar registrada")
-	public void testAddServiceNullRuta() throws Exception {
+	@Test
+	public void testAddServiceNullRuta(){
+		when(rutasRepository.getRutas(ruta2.getId())).thenReturn(null);
+		when(busesRepo.getBus(bus1.getId())).thenReturn(bus1);
+		when(conductoresRepo.getConductor(conductore1.getCedula())).thenReturn(conductore1);
+		
 		Tmio1Servicio serv = new Tmio1Servicio();
 		serv.setNewId("0");
 		serv.setTmio1Bus(bus1);
 		serv.setTmio1Conductore(conductore1);
-		serv.setTmio1Ruta(null);
-		service.addServicio(serv);
+		serv.setTmio1Ruta(ruta2);
+		try {
+			service.addServicio(serv);
+			fail();
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "La ruta debe de estar registrada");
+			verify(conductoresRepo).getConductor(conductore1.getCedula());
+			verify(busesRepo).getBus(bus1.getId());
+			verify(rutasRepository).getRutas(ruta2.getId());
+			
+			verifyNoMoreInteractions(repository);
+			verifyNoMoreInteractions(rutasRepository);
+			verifyNoMoreInteractions(busesRepo);
+			verifyNoMoreInteractions(conductoresRepo);
+		}
 	}
 	
 	@Test(expectedExceptions = NullPointerException.class,
